2c76d9fca42b147e51e1c2b4f2ea023e78e44ddf
Kilder Costa <costa.kilder@live.com>
2024-08-02 14:12:24 -0300

    style: sheet

--- src/app/(main)/trips/[id]/_components/header/header-client.tsx
+++ src/app/(main)/trips/[id]/_components/header/header-client.tsx
@@ -1,11 +1,20 @@
 'use client'
 
-import { UserButton } from '@clerk/nextjs'
+import { SignOutButton, UserButton } from '@clerk/nextjs'
 import { useMediaQuery } from '@uidotdev/usehooks'
-import { Calendar, MapPin, Menu } from 'lucide-react'
+import { ArrowRightFromLine, Calendar, MapPin, Menu } from 'lucide-react'
+import Image from 'next/image'
 import { useEffect } from 'react'
 
 import { Button } from '@/components/ui/button'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 import { useGetTrip } from '@/hooks/trips'
 import { useCurrentTripStore } from '@/stores/current-trip'
 import { formatDateRange } from '@/utils/format-date-range'
@@ -62,9 +71,43 @@ export function HeaderClient({ tripId }: Props) {
           <UserButton />
         </div>
 
-        <Button variant="secondary" className="md:hidden" size={'icon2'}>
-          <Menu className="size-5" />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="secondary" className="md:hidden" size={'icon2'}>
+              <Menu className="size-5" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="flex flex-col">
+            <SheetHeader>
+              <SheetTitle>
+                <Image
+                  src="/logo.svg"
+                  alt="Plann.er"
+                  className="h-16"
+                  width={200}
+                  height={100}
+                />
+              </SheetTitle>
+              <SheetDescription></SheetDescription>
+            </SheetHeader>
+            <div className="flex flex-col flex-1">
+              <div className="flex-1">
+                <h4 className="text-medium font-medium text-zinc-200">
+                  Minhas viagens planejadas
+                </h4>
+              </div>
+              <div className="flex gap-3">
+                <UserButton />
+                <SignOutButton>
+                  <Button size={'sm'} variant={'outline'}>
+                    <p className="text-destructive">Sair</p>
+                    <ArrowRightFromLine className="size-5 text-destructive" />
+                  </Button>
+                </SignOutButton>
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   )
