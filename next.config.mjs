07f090aa28e8b3a73b4eb193932cbf4090a3e8eb
Kilder Costa <costa.kilder@live.com>
2024-08-08 13:14:27 -0300

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   logging: {
     fetches: {
       fullUrl: true,
     },
   },
-  output: 'standalone',
 }
 
 export default nextConfig
